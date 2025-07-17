915c60e0841b919601b56c31d775f7c162cc2fb9
Fatemezri <zarei.fateme937@gmail.com>
2025-07-17 07:59:28 +0330

    user (test_1) add: creating CustomUser model with email and phone fields

--- user/models.py
+++ user/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email=None, phone=None, password=None, **extra_fields):
+        if not email and not phone:
+            raise ValueError("شماره همراه یا ایمیل خود را وارد کنید")
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, phone=phone, **extra_fields)
+        user.set_password(password) #هش کردن رمز
+        user.save()
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(username, email=email, password=password, **extra_fields)
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(unique=True, null=True, blank=True) # unique:ایمیل تکراری نباشه null:فیلد میتونه خالی باشه blanck: فرم میتونه خالی باشه
+    phone = models.CharField(max_length=20, unique=True, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'username'  # هنوز برای createsuperuser لازمه
+    REQUIRED_FIELDS = ['email']  # برای ساختن ادمین
+
+    def __str__(self):
+        return self.username
+
 
 
