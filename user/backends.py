12a1b9388f7a12dcff0e165647f0b8f79e212bae
Fatemezri <zarei.fateme937@gmail.com>
2025-07-17 09:13:29 +0330

    user(test_1)add:implement custom authentication backend

---
+++ user/backends.py
@@ -0,0 +1,25 @@
+#چون نمیخواهیم با username  لاگین کنیم یک custom authentication backend تعریف میکنیم
+
+from django.contrib.auth.backends import BaseBackend
+from django.contrib.auth import get_user_model
+
+UserModel = get_user_model()
+
+
+class UsernameAndContactBackend(BaseBackend):
+    def authenticate(self, request, username=None, contact=None, password=None, **kwargs):
+        try:
+            if '@' in contact:
+                user = UserModel.objects.get(username=username, email=contact)
+            else:
+                user = UserModel.objects.get(username=username, phone=contact)
+            if user.check_password(password):
+                return user
+        except UserModel.DoesNotExist:
+            return None
+
+    def get_user(self, user_id):
+        try:
+            return UserModel.objects.get(pk=user_id)
+        except UserModel.DoesNotExist:
+            return None
