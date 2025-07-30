f336def52ca8e625631d884981ef5e64e8d0371b
Fatemezri <zarei.fateme937@gmail.com>
2025-07-30 15:15:39 +0330

    edit setting

--- website/settings.py
+++ website/settings.py
@@ -145,9 +145,9 @@ USE_TZ = True
 
 
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = 'static/'
