cce92546e7a5a38c28ae009b7166c0f8ef72e504
Fatemezri <zarei.fateme937@gmail.com>
2025-07-31 10:36:24 +0330

    login(main)add:edit setting for send the Email

--- website/settings.py
+++ website/settings.py
@@ -185,7 +185,7 @@ SOCIAL_AUTH_PIPELINE = (
 EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
-EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "False") == "False"
 EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "True") == "True"
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
