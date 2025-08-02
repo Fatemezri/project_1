31cc4c7a567c574f45e6fa5a60027577c4fcf0a1
Fatemezri <zarei.fateme937@gmail.com>
2025-08-02 15:30:50 +0330

    comments(main)add: add Comment model and initial migrations add register Comment model in admin panel

--- website/urls.py
+++ website/urls.py
@@ -1,17 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import views
-from comments.views import submit_comment, admin_panel, approve_comment
-
+from comments.admin_site import moderator_admin_site
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("manager/", views.manager_dashboard),
-    path("comments/", include("comments.urls")),
     path('', include('user.urls')),
     path('accounts/', include('allauth.urls')),
-
+    path('moderator-admin/', moderator_admin_site.urls),
+    path('comments/', include('comments.urls')),
 
 ]
 
