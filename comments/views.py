a8bec6661310af2bc562d797231b9ae8456ab0b3
Fatemezri <zarei.fateme937@gmail.com>
2025-08-03 20:02:16 +0330

    comment(main) fix:update  comment app

--- comments/views.py
+++ comments/views.py
@@ -1,15 +1,18 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect
 from django.contrib import messages
+from .models import Comment, Notification
 from django.contrib.auth import get_user_model
-from .models import Comment, Report  # مدل Notification به Report تغییر نام یافته است.
-
 User = get_user_model()
-
+from django.shortcuts import render
+from .models import Comment
 
 def comment_list(request):
     approved_comments = Comment.objects.filter(status='approved').order_by('-created_at')
     return render(request, 'comments/comment_list.html', {'comments': approved_comments})
+
+
+
 
 
 @login_required
@@ -19,13 +22,13 @@ def submit_comment(request):
         if text:
             comment = Comment.objects.create(user=request.user, text=text)
 
-            # ارسال گزارش به اولین ناظر پیدا شده
+            # ارسال اعلان به اولین ناظر پیدا شده
             moderator = User.objects.filter(groups__name='Moderators', is_active=True).first()
             if moderator:
-                Report.objects.create(  # نام مدل به Report تغییر یافته است
+                Notification.objects.create(
                     recipient=moderator,
                     sender=request.user,
-                    report_type='new_comment',  # نوع گزارش را new_comment قرار می‌دهیم
+                    notification_type='new_comment',
                     message=f"یک نظر جدید از طرف {request.user.username} ثبت شد.",
                     related_comment=comment
                 )
@@ -34,8 +37,5 @@ def submit_comment(request):
         else:
             messages.error(request, "متن نظر نمی‌تواند خالی باشد.")
 
-        # پس از ارسال موفقیت‌آمیز فرم، به یک صفحه دیگر هدایت شوید
-        # نام view یا URL مورد نظر خود را جایگزین کنید
-        return redirect('some_other_view_or_homepage')
-
     return render(request, 'comment/submit.html')
+    return redirect('some_other_view_or_homepage')  # آدرس مورد نظر خود را جایگزین کنی
