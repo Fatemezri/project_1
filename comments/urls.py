a66a8b00053175dae155f85ae75a940f2f27642d
Fatemezri <zarei.fateme937@gmail.com>
2025-08-03 21:08:18 +0330

    comment(main) delete:delete comment app

--- comments/urls.py
+++
@@ -1,8 +0,0 @@
-# comments/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('list/', views.comment_list, name='comment_list'),
-    path('submit/', views.submit_comment, name='submit_comment'),
-]
