20adf5b30ddb86d046d22951095bb4fe7774a055
Fatemezri <zarei.fateme937@gmail.com>
2025-08-03 12:18:30 +0330

    comment(main) fix:update  comment app

--- comments/urls.py
+++ comments/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views
-from .admin_site import moderator_admin_site
 
 app_name = 'comments'
 urlpatterns = [
     path('submit-comment/', views.submit_comment, name='submit_comment'),
-    path('moderator-admin/', moderator_admin_site.urls),
 ]
