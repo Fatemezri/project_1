8156a4f445fb6ee8f7cbdf40518cbd9ed97fb427
Fatemezri <zarei.fateme937@gmail.com>
2025-08-05 13:42:47 +0330

    report_app(main) fix:edit context_processors.py

--- report_app/context_processors.py
+++ report_app/context_processors.py
@@ -6,5 +6,5 @@ def unread_messages_count(request):
     if request.user.is_authenticated and request.user.is_superuser:
         unread_count = Message.objects.filter(recipient=request.user, is_read=False).count()
         # دستور اشکال‌زدایی: تعداد پیام‌های خوانده نشده را در کنسول نمایش می‌دهد.
-        print(f"تعداد پیام‌های خوانده نشده برای سوپریوزر {request.user.username}: {unread_count}")
+        print(f"تعداد پیام‌های خوانده نشده برای سوپریوزر {request.user.username}: {unread_count}".encode('utf-8'))
     return {'unread_messages_count': unread_count}
