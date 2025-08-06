8a0b7cc1f34caf190bc0bf140f405c263e742052
Fatemezri <zarei.fateme937@gmail.com>
2025-08-06 20:42:08 +0330

    .

--- report_app/context_processors.py
+++ report_app/context_processors.py
@@ -1,13 +1,7 @@
 from .models import Message
-import sys
 
-# تنظیم کدک خروجی به utf-8 برای جلوگیری از UnicodeEncodeError
-sys.stdout.reconfigure(encoding='utf-8')
-
-def unread_messages_count(request):
-    """پردازنده محتوا برای اضافه کردن تعداد پیام‌های خوانده نشده به محتوای ادمین."""
-    unread_count = 0
+def unread_message_count(request):
     if request.user.is_authenticated and request.user.is_superuser:
-        unread_count = Message.objects.filter(recipient=request.user, is_read=False).count()
-        # دیباگ – نمایش در ترمینال
-    return {'unread_messages_count': unread_count}
+        count = Message.objects.filter(is_read=False).count()
+        return {'unread_count': count}
+    return {}
